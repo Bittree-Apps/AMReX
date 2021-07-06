b74ab22fe77bc36ee7e64ad45c343af6e9625777
akashdhruv <akashdhruv@gwmail.gwu.edu>
2021-07-06 18:08:29 -0400

    callback design ready for testing

--- Src/F_Interfaces/AmrCore/AMReX_bittree_mod.F90
+++ Src/F_Interfaces/AmrCore/AMReX_bittree_mod.F90
@@ -10,9 +10,11 @@ module amrex_bittree_module
   public :: amrex_bittree_procedure
 
   interface
-     subroutine amrex_bittree_procedure (bittree_flg) bind(c)
+     subroutine amrex_bittree_procedure (lbase,time,new_finest,new_grid,bittree_dmap) bind(c)
        import
-       integer(c_int), value :: bittree_flg
+       integer(c_int), value :: lbase
+       real(amrex_real), value :: time
+       type(c_ptr), value :: new_finest,new_grid,bittree_dmap      
      end subroutine amrex_bittree_procedure
   end interface
 

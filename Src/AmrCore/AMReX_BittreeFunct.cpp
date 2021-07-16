6db236586977bd7513be8a885ff4ad110420210b
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2021-07-16 10:55:38 -0400

    formatting the code design

--- Src/AmrCore/AMReX_BittreeFunct.cpp
+++ Src/AmrCore/AMReX_BittreeFunct.cpp
@@ -3,10 +3,10 @@
 using namespace amrex;
 
 void
-amrex::BittreeFunct::operator() (int lev, Real time, int& new_finest, BoxArray& new_grids, DistributionMapping& bittree_dmap)
+amrex::BittreeFunct::operator() (int lev, Real time, int& new_finest, BoxArray& bittree_grid, DistributionMapping& bittree_dmap)
 {
     if (bittree_func != nullptr) {
-        bittree_func (lev,time,new_finest,new_grids,bittree_dmap);
+        bittree_func (lev,time,new_finest,bittree_grid,bittree_dmap);
     } else {
         amrex::Abort("BittreeFunct::bittree_func is null");
     }

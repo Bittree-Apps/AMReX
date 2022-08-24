b045350a7a17a37dd839b0d692740ef7c6b36c48
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2022-08-23 22:17:22 -0500

    organize bittree code and introduce bittree directives in GNUmake build system

--- Src/AmrCore/AMReX_AmrMesh.cpp
+++ Src/AmrCore/AMReX_AmrMesh.cpp
@@ -6,7 +6,9 @@
 #include <AMReX_ParallelDescriptor.H>
 #include <AMReX_Print.H>
 
+#ifdef AMREX_USE_BITTREE
 #include <AMReX_Bittree.H>
+#endif
 
 #include <memory>
 
@@ -382,7 +384,10 @@ AmrMesh::InitAmrMesh (int max_level_in, const Vector<int>& n_cell_in,
 
     if (check_input) { checkInput(); }
 
+#ifdef AMREX_USE_BITTREE
     pp.get("use_bittree",use_bittree);
+#endif
+
 }
 
 int
@@ -520,7 +525,9 @@ AmrMesh::MakeNewGrids (int lbase, Real time, int& new_finest, Vector<BoxArray>& 
 
     if (new_grids.size() < max_crse+2) new_grids.resize(max_crse+2);
 
+#ifdef AMREX_USE_BITTREE
     if(!use_bittree) {
+#endif
 
     //
     // Construct problem domain at each level.
@@ -782,8 +789,12 @@ AmrMesh::MakeNewGrids (int lbase, Real time, int& new_finest, Vector<BoxArray>& 
             }
         }
     }
-    }
 
+#ifdef AMREX_USE_BITTREE
+    }
+#endif
+
+#ifdef AMREX_USE_BITTREE
     // Bittree version
     if(use_bittree) {
         // Initialize BT refinement
@@ -1125,6 +1136,8 @@ AmrMesh::MakeNewGrids (int lbase, Real time, int& new_finest, Vector<BoxArray>& 
 =======
 >>>>>>> Bittree doesn't need to make dm
     }
+#endif
+
 }
 
 void
@@ -1139,9 +1152,13 @@ AmrMesh::MakeNewGrids (Real time)
         const auto old_num_setdm = num_setdm;
         const auto old_num_setba = num_setba;
 
+#ifdef AMREX_USE_BITTREE
         if(!use_bittree) {
+#endif
             ba = MakeBaseGrids();
             dm = DistributionMapping(ba);
+
+#ifdef AMREX_USE_BITTREE
         }
         else {
             //Initialize Bittree
@@ -1171,7 +1188,7 @@ AmrMesh::MakeNewGrids (Real time)
             btUnit::btCalculateLevel(btmesh.get(),0,ba,max_grid_size[0]);
             dm = DistributionMapping(ba);
         }
-
+#endif
         MakeNewLevelFromScratch(0, time, ba, dm);
 
         if (old_num_setba == num_setba) {
